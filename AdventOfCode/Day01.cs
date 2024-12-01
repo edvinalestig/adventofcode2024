9b5333f82cbf233cc680158f6786478bf9ab5878
Edvin Alestig <edvin.alestig@gmail.com>
2024-12-01 14:12:42 +0100

    Day 1

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -9,7 +9,46 @@ public class Day01 : BaseDay
         _input = File.ReadAllText(InputFilePath);
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+    public override ValueTask<string> Solve_1()
+    {
+        List<int> leftList = [];
+        List<int> rightList = [];
+        foreach (string line in _input.Split("\n", StringSplitOptions.RemoveEmptyEntries))
+        {
+            // Parse into lists
+            string[] splat = line.Split("   ");
+            leftList.Add(int.Parse(splat[0]));
+            rightList.Add(int.Parse(splat[1]));
+        }
 
-    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+        leftList.Sort();
+        rightList.Sort();
+        int distance = 0;
+
+        for (int i = 0; i < leftList.Count; i++)
+        {
+            distance += Math.Abs(leftList[i] - rightList[i]);
+        }
+
+        return new(distance.ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        List<int> leftList = [];
+        List<int> rightList = [];
+        foreach (string line in _input.Split("\n", StringSplitOptions.RemoveEmptyEntries))
+        {
+            string[] splat = line.Split("   ");
+            leftList.Add(int.Parse(splat[0]));
+            rightList.Add(int.Parse(splat[1]));
+        }
+
+        // Get dictionary of number in right list: occurences in right list 
+        Dictionary<int, int> occurences = rightList.GroupBy(i => i).ToDictionary(g => g.Key, g => g.Count());
+        // Iterate over left list and sum number in left list * occurences in right list
+        int score = leftList.Aggregate(0, (total, left) => total + left * occurences.GetValueOrDefault(left, 0));
+
+        return new(score.ToString());
+    }
 }
